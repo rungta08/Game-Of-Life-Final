2e75f1083196356721ab47f66ec6a6bfa67a2a20
Shivam.rungta <shivam08rungta@gmail.com>
2019-07-06 22:22:27 +0530

    [Shivam]Add. Size of world input field

--- src/universe/Filter/Filter.js
+++ src/universe/Filter/Filter.js
@@ -4,8 +4,16 @@ import PropTypes from 'prop-types';
 class Filter extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="Filter">
+                <label>Size: </label><input id='size-input'
+                        type = 'number'
+                        min={0}
+                        max={20}
+                        defaultValue={10}
+                        onChange = {this.changeSize}/>
+                <br/>
+                <br/>
+
             </div>
         );
     }
