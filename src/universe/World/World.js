d61433839f39f9f6ff52d8a46c6a5cbb4f7ca20c
Shivam.rungta <shivam08rungta@gmail.com>
2019-07-06 22:08:40 +0530

    [Shivam]Add. Change no of cell displayed dynamically

--- src/universe/World/World.js
+++ src/universe/World/World.js
@@ -10,6 +10,36 @@ class World extends React.Component {
             aliveCells: Array(this.props.size).fill(0),
         }
     }
+
+    reverse = (value) => {
+        if(value === 0)
+            return 1;
+        return 0;
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        let aliveCells = this.state.aliveCells;
+        if(parseInt(nextProps.size) > this.state.aliveCells.length){
+            aliveCells.push(0);
+        }
+        if(parseInt(nextProps.size) < this.state.aliveCells.length){
+            aliveCells.pop();
+        }
+
+        this.setState({
+            aliveCells: aliveCells
+        },() => {
+            console.log(this.state.aliveCells)
+        })
+    }
+
+    addToLiveCells = (id) => {
+        const aliveCells = this.state.aliveCells;
+        aliveCells[id] = reverse(aliveCells[id]);
+        this.setState({
+            aliveCells : aliveCells
+        });
+    };
     render() {
         let cells = [];
         for(let i = 0; i < this.props.size;i++){
